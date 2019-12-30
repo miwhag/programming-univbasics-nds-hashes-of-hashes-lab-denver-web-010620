703bd7b82ccb73faea78eb7b1cb7443a3fb2c2bf
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 21:38:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,11 @@ country = {
                 :sub_category => {
                   :label => "Family",
                     :sub_category => {
-                      :label => "Genus"
+                      :label => "Genus",
+                        :sub_category => {
+                          :label => "Species", 
+                            :sub_category => nil 
+                        }
                     }
                 }
           }
