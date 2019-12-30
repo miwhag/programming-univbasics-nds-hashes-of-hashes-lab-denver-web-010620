e38bae647ac5f4b1679443ec69955f19f7b6629b
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 21:31:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,7 +36,10 @@ country = {
     :sub_category => {
       :label => "Phylum",
        :sub_category => {
-         :label => "Class"
+         :label => "Class",
+          :sub_category => {
+              :label => "Order"
+          }
        }
      }
    }
